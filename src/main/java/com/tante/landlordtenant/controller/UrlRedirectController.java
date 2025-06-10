0f91b16db6fcd7d514d1a354cafcb070b535e15b
AzureAD\ManenoMgwami <Maneno.Mgwami@osiristrading.com>
2025-06-10 19:50:46 +0200

    added pipeline

---
+++ src/main/java/com/tante/landlordtenant/controller/UrlRedirectController.java
@@ -0,0 +1,22 @@
+package com.tante.landlordtenant.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/redirect")
+@RestController
+@CrossOrigin(origins = "*")
+public class UrlRedirectController {
+
+    @GetMapping
+    public ResponseEntity<Void> redirect() {
+        String amplifyUrl = "https://www.tante.tz/";
+        return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).header("Location", amplifyUrl).build();
+    }
+
+}
